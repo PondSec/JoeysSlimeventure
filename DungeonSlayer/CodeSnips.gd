0a4bb0db5db055aae997f65028bac445193df25d
Joshua Pond <joshua@pondsec.de>
2024-11-11 11:25:28 +0100

    Add files via upload

--- DungeonSlayer/CodeSnips.gd
+++ DungeonSlayer/CodeSnips.gd
@@ -1,48 +1,19 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+const GRAVITY = 5
+const SPEED = 500
 
-# Variable für die Laufrichtung
-var direction := Vector2.ZERO
-
-func _physics_process(delta: float) -> void:
-	# Gravitation anwenden, wenn nicht auf dem Boden
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Sprung ausführen, wenn die Taste gedrückt wird und auf dem Boden
-	if Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Horizontale Bewegung basierend auf der Eingabe
-	direction.x = Input.get_axis("left", "right")
-	if direction.x != 0:
-		velocity.x = direction.x * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	# Bewegen und gleiten lassen
-	move_and_slide()
-
-	# Animation einstellen
-	set_animation()
-
-# Funktion zum Einstellen der Animation
-func set_animation():
-	if direction.x < 0:
-		$PlayerSprite.flip_h = true
-		$AnimationPlayer.play("walk")
-		
-	elif direction.x > 0:
-		$PlayerSprite.flip_h = false
-		$AnimationPlayer.play("walk")
+func _process(delta):
+	var move = Vector2(0, GRAVITY)
 	
-	if direction.x == 0:
-		$AnimationPlayer.play("idle")
+	if Input.is_action_pressed("left"):
+		move.x = -1
+	
+	if Input.is_action_pressed("right"):
+		move.x = 1
 		
-	if is_in_air():
-		$AnimationPlayer.play("jump")
+	if Input.is_action_pressed("up"):
+		move.y = -8
 
-func is_in_air():
-	return is_on_floor() == false
+	velocity = move * SPEED
+	move_and_slide()
