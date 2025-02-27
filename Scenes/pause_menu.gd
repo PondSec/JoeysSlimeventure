4fd5dd691992a220bc3b8938433200530ff28fa7
Joshua Pond <joshua@pondsec.de>
2025-02-27 21:24:54 +0100

    Added Inventory System

--- Scenes/pause_menu.gd
+++ Scenes/pause_menu.gd
@@ -6,15 +6,20 @@ signal go_to_main_menu
 func _ready():
 	# Standardmäßig ausblenden
 	visible = false
+	# Sicherstellen, dass das Pausenmenü die höchste Rendering-Priorität hat
+	set_layer(100)  
 
 func toggle_pause():
 	# Pausenmenü anzeigen oder ausblenden
 	visible = not visible
 	get_tree().paused = visible
 
+	# Stelle sicher, dass das Pausenmenü beim Öffnen ganz oben gerendert wird
+	if visible:
+		get_parent().move_child(self, get_parent().get_child_count() - 1)
+
 func _on_continue_button_pressed() -> void:
 	toggle_pause()
-
 
 func _on_main_menu_button_pressed() -> void:
 	emit_signal("go_to_main_menu")
