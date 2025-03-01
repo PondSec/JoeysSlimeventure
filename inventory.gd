7f8f5d36fefa7f4cc4d1bf25b55576345c504bc6
Joshua Pond <joshua@pondsec.de>
2025-03-01 13:47:20 +0100

    Added Drag and Drop System. Item stay in the right Slot if closing the Inventory and reopen it.

--- inventory.gd
+++ inventory.gd
@@ -6,7 +6,6 @@ signal update
 
 @export var slots: Array[InvSlot]
 
-
 func Insert(item: InvItem):
 	var itemslots = slots.filter(func(slot): return slot.item == item)
 	if !itemslots.is_empty():
@@ -15,5 +14,12 @@ func Insert(item: InvItem):
 		var emptyslots = slots.filter(func(slot): return slot.item == null)
 		if !emptyslots.is_empty():
 			emptyslots[0].item = item
-			emptyslots[0].amount  = 1
+			emptyslots[0].amount = 1
+	update.emit()
+
+# Swap items between two slots
+func swap_slots(index1: int, index2: int):
+	var temp = slots[index1]
+	slots[index1] = slots[index2]
+	slots[index2] = temp
 	update.emit()
