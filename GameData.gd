7480a4b886c454886db2f8403dbd7c810b6e9306
Joshua Pond <joshua@pondsec.de>
2025-03-02 19:03:24 +0100

    Added Death Screen. Added copper_nugget, iron_nugget. Added Drop Inventory while death. Added Right click du drop item once. added right click on a stack to grap the half stack.

--- GameData.gd
+++ GameData.gd
@@ -5,6 +5,7 @@ class_name GameData
 @export var is_facing_left: bool
 @export var is_stunned: bool
 @export var is_glowing: bool
+@export var is_glowing_visible: bool
 @export var current_health: int
 @export var max_health: int
 @export var attack_damage: int
