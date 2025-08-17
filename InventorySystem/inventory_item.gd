9d7d2e95415f8b2e1984335549ca4e4259a54e5c
Joshua Pond <joshua@pondsec.com>
2025-08-17 18:53:40 +0200

    Added more buffs

--- InventorySystem/inventory_item.gd
+++ InventorySystem/inventory_item.gd
@@ -5,3 +5,6 @@ class_name InvItem
 @export var name: String = ""
 @export var texture: Texture2D
 @export var health_bonus: float = 0 # in %
+@export var damage_bonus: float = 0 # in %
+@export var crit_chance_bonus: float = 0  # in % (z.B. 0.05 für +5%)
+@export var crit_damage_bonus: float = 0  # in % (z.B. 0.1 für +10% Crit-Schaden)
