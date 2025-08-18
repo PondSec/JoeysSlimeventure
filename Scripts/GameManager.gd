fd15fc9dd7a3850a32f9789fbd555d31bc3ca398
Joshua Pond <joshua.pond@obsbs.de>
2025-08-18 14:19:41 +0200

    Added Joined Player to working tree

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,19 +1,34 @@
 extends Node
 
-# Netzwerk-Funktionen
-var current_player_scene = preload("res://Scenes/player.tscn")
-
 func host_game(port):
 	var peer = ENetMultiplayerPeer.new()
-	peer.create_server(port)
+	var err = peer.create_server(port)
+	if err != OK:
+		print("Fehler beim Erstellen des Servers: ", err)
+		return
+	
 	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(_on_peer_connected)
 	load_game_world()
 
 func join_game(ip, port):
 	var peer = ENetMultiplayerPeer.new()
-	peer.create_client(ip, port)
+	var err = peer.create_client(ip, port)
+	if err != OK:
+		print("Fehler beim Verbinden: ", err)
+		return
+	
 	multiplayer.multiplayer_peer = peer
-	# Welt wird geladen wenn Verbindung steht
+	# Warte auf Verbindung bevor die Welt geladen wird
+	peer.connection_succeeded.connect(_on_connection_success.bind(ip, port))
+
+func _on_connection_success(ip, port):
+	print("Erfolgreich verbunden mit ", ip, ":", port)
+	load_game_world()
+
+func _on_peer_connected(peer_id):
+	print("Neuer Spieler verbunden: ", peer_id)
 
 func load_game_world():
+	# Lade die Welt für alle Spieler
 	get_tree().change_scene_to_file("res://Scenes/world.tscn")
